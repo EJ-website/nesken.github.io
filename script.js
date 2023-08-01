c5a1811e771734bd41dd767a03f6361140d7bde4
EJ-website <enderjhass@gmail.com>
2023-08-01 19:39:20 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", function() {
+    nav.classList.toggle("sticky", window.scrollY > 80)
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
